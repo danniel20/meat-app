6a276d21982930594fc68ef854d8760166288b92
Danniel Albuquerque <danniel.master@gmail.com>
2018-03-31 17:58:20 -0300

    adicionando navegação programática via Router

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import {Router} from '@angular/router'
+
 import { RadioOption } from '../shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
@@ -19,7 +21,7 @@ export class OrderComponent implements OnInit {
 		{label: 'Cartão Refeição', value: 'REF'}
 	]
 
-  constructor(private orderService: OrderService) { }
+  constructor(private orderService: OrderService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -47,7 +49,7 @@ export class OrderComponent implements OnInit {
   checkOrder(order: Order){
     order.orderItems = this.cartItems().map((item: CartItem) => new OrderItem(item.quantity, item.menuItem.id))
     this.orderService.checkOrder(order).subscribe((orderId: string) => {
-      console.log(`Compra concluída: ${orderId}`)
+      this.router.navigate(['/order-summary'])
       this.orderService.clear()
     })
   }
